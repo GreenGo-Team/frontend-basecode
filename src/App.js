268d2f9de5f944ab6900e15630ae75e3ab7f17dd
sara <sara2003me@gmail.com>
2024-09-12 19:31:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route,Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import SignUp from './components/profile/SignUp.jsx';
 import Loadingpage from './components/Loadingpage';
@@ -12,24 +12,24 @@ import Profile from './components/profile/Profile.jsx';
 import Signin from './components/profile/Signin.jsx';
 import Learndetails from './components/learn/Learndetails.jsx';
 
+
+
 function App() {
   return (
     <div className="App">
-      
-      <Routes>
-        <Route path='/' element={<Loadingpage/>}/>
-        <Route path='/homepage' element={<Homepage/>}></Route>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/rewards' element={<Rewards/>}/>
-        <Route path='/scan' element={<Scan/>}/>
-        <Route path='/scan/scaned' element={<Scaned/>}/>
-        <Route path='/scanedlocation' element={<ScanedLocation/>}/>
-        <Route path='/learn' element={<Learn />}/>
-        <Route path='/profile' element={<Profile />}/>
-        <Route path='/signin' element={<Signin/>}/>
-        <Route path='/details' element={<Learndetails/>}/>
-
-      </Routes>
+        <Routes>
+          <Route path='/frontend-basecode' element={<Loadingpage />} />
+          <Route path='/homepage' element={<Homepage />}></Route>
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/rewards' element={<Rewards />} />
+          <Route path='/scan' element={<Scan />} />
+          <Route path='/scan/scaned' element={<Scaned />} />
+          <Route path='/scanedlocation' element={<ScanedLocation />} />
+          <Route path='/learn' element={<Learn />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/details' element={<Learndetails />} />
+        </Routes>
     </div>
   );
 }
